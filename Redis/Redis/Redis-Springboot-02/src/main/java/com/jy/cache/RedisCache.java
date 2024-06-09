8098d0fa6dc516b14a283b79116a80805cccb4b9
fly-1-1 <2673024411@qq.com>
2024-06-10 05:41:09 +0800

    Redis 分布式缓存3 穿透 击穿 雪崩

--- Redis/Redis/Redis-Springboot-02/src/main/java/com/jy/cache/RedisCache.java
+++ Redis/Redis/Redis-Springboot-02/src/main/java/com/jy/cache/RedisCache.java
@@ -4,6 +4,9 @@ import com.jy.util.ApplicationContextUtils;
 import org.apache.ibatis.cache.Cache;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.DigestUtils;
+
+import java.util.concurrent.TimeUnit;
 
 public class RedisCache implements Cache {
 
@@ -25,22 +28,24 @@ public class RedisCache implements Cache {
 
         System.out.println("key = " + key + ", value = " + value);
         //redis中的hash缓存
-        getRedisTemplate().opsForHash().put(id.toString(), key.toString(), value);
+        getRedisTemplate().opsForHash().put(getKey2Md5(this.id.toString()), key.toString(), value);
+
+
+        if (id.equals("com.jy.mapper.UserMapper")){
+            getRedisTemplate().expire(id.toString(),1, TimeUnit.MINUTES);
+        }
     }
 
     @Override
     public Object getObject(Object key) {
 
 
-        return getRedisTemplate().opsForHash().get(id.toString(),key.toString());
+        return getRedisTemplate().opsForHash().get(id.toString(),getKey2Md5(key.toString()));
     }
 
     @Override
     public Object removeObject(Object key) {
         System.out.println("根据指定的key删除缓存");
-
-
-
         return null;
     }
 
@@ -60,6 +65,11 @@ public class RedisCache implements Cache {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    //封装一个对key进行md5加密的工具方法
+    private String getKey2Md5(String key){
+        return DigestUtils.md5DigestAsHex(key.getBytes());
     }
 
 
--- Redis/Redis/Redis-Springboot-02/src/test/java/com/jy/TestEmpService.java
+++ Redis/Redis/Redis-Springboot-02/src/test/java/com/jy/TestEmpService.java
@@ -6,6 +6,7 @@ import com.jy.service.IEmpService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
 
 @SpringBootTest
 public class TestEmpService {
@@ -19,8 +20,13 @@ public class TestEmpService {
         empService.list().forEach(System.out::println);
         System.out.println("=============");
         empService.list().forEach(System.out::println);
+    }
 
-
+    @Test
+    public void testMd5(){
+        String key="1860238455:4679889258:com.jy.mapper.EmpMapper.selectList:0:2147483647:SELECT  id,name  FROM emp:MybatisSqlSessionFactoryBean";
+        String string = DigestUtils.md5DigestAsHex(key.getBytes());
+        System.out.println(string);
     }
 
 }
